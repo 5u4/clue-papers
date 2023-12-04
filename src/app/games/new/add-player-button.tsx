caf063d4d2bcc982c4e348daec48fd11f37df2d0
Sen Hung Wong <alexwongsenhung@gmail.com>
2023-12-03 23:39:54 -0800

    Update add player button

--- src/app/games/new/add-player-button.tsx
+++ src/app/games/new/add-player-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useAtomValue, useSetAtom } from "jotai/react";
+import { useAtomValue } from "jotai/react";
 
 import { Button } from "~/components/ui/button";
 import {
@@ -17,7 +17,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
-import { addPlayerActionAtom, playersReadOnlyAtom } from "~/data/players-store";
+import { playersReadOnlyAtom } from "~/data/players-store";
 import { notEmpty } from "~/utils/not-empty";
 import { cn } from "~/utils/ui";
 
@@ -31,90 +31,63 @@ export const AddPlayerButton: React.FC<Props> = ({
   onAddPlayerName,
 }) => {
   const players = useAtomValue(playersReadOnlyAtom);
-  const addPlayer = useSetAtom(addPlayerActionAtom);
 
   const [name, setName] = useState<string>();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState<string>();
 
-  const onAddPlayer = (name: string) => {
-    if (players.every((p) => p !== name)) {
-      addPlayer(name);
-    }
-    onAddPlayerName(name);
-    setName("");
-  };
-
   return (
     <div className="grid w-full items-center gap-1.5">
       <Label htmlFor="name">Player Name</Label>
-      <div className="flex flex-row space-x-2 items-center justify-between">
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              className="w-full"
-              disabled={selectedPlayerNames.length >= 6}
-            >
-              {name ? <>{name}</> : <>+ Player</>}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="p-0" align="start">
-            <Command>
-              <CommandInput
-                placeholder="Type in player name"
-                value={text}
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-                onInput={(e) => setText((e.target as any).value)}
-              />
-              <CommandList>
-                <CommandGroup>
-                  {[
-                    players.some((p) => p === text) ? undefined : text,
-                    ...players,
-                  ]
-                    .filter(notEmpty)
-                    .map((player) => (
-                      <CommandItem
-                        className={cn(
-                          selectedPlayerNames.some((p) => p === player) &&
-                            "opacity-30",
-                        )}
-                        key={player ?? ""}
-                        value={player}
-                        onSelect={(value) => {
-                          if (players.some((p) => p === value)) {
-                            /** directly add to list if is known player */
-                            onAddPlayer(value);
-                          } else {
-                            /** wait for user to confirm add player if is new player */
-                            setName(value);
-                          }
-                          setText("");
-                          setOpen(false);
-                        }}
-                        disabled={selectedPlayerNames.some((p) => p === player)}
-                      >
-                        {player}
-                      </CommandItem>
-                    ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
-
-        <Button
-          disabled={
-            !name ||
-            selectedPlayerNames.some((p) => p === name) ||
-            selectedPlayerNames.length >= 6
-          }
-          onClick={() => onAddPlayer(name!)}
-        >
-          Add Player
-        </Button>
-      </div>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            className="w-full"
+            disabled={selectedPlayerNames.length >= 6}
+          >
+            {name ? <>{name}</> : <>+ Player</>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0" align="start">
+          <Command>
+            <CommandInput
+              placeholder="Type in player name"
+              value={text}
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+              onInput={(e) => setText((e.target as any).value)}
+            />
+            <CommandList>
+              <CommandGroup>
+                {[
+                  players.some((p) => p === text) ? undefined : text,
+                  ...players,
+                ]
+                  .filter(notEmpty)
+                  .map((player) => (
+                    <CommandItem
+                      className={cn(
+                        selectedPlayerNames.some((p) => p === player) &&
+                          "opacity-30",
+                      )}
+                      key={player ?? ""}
+                      value={player}
+                      onSelect={(value) => {
+                        onAddPlayerName(value);
+                        setName("");
+                        setText("");
+                        setOpen(false);
+                      }}
+                      disabled={selectedPlayerNames.some((p) => p === player)}
+                    >
+                      {player}
+                    </CommandItem>
+                  ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
